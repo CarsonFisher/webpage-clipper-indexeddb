e230bf8a71dbb3ca0cbeae8344af0c49fae647d8
Carson Fisher <candlfish3@gmail.com>
2025-04-15 13:23:06 -0400

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -16,6 +16,7 @@ async function initDB() {
   
   // Handle database upgrade (called when DB is created or version changes)
   request.onupgradeneeded = (event) => {
+    const DB_VERSION = 2; // Increment from 1 to 2
     const db = event.target.result;
     
     // Create the object store (table) if it doesn't exist
